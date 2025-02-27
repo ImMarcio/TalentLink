9e15a56199c36fd2ebb556c8028ae593715d87a8
Márcio J. Silva <105254896+ImMarcio@users.noreply.github.com>
2025-02-27 16:56:37 -0300

    feat: validações para empresa, vagas e candidatos. popup  de erros no cadastro

--- app/models/candidato.rb
+++ app/models/candidato.rb
@@ -5,12 +5,17 @@ class Candidato < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   # Validações
-  validates :nome, presence: true
-  validates :telefone, presence: true
-  validates :linkedin, presence: true
-  validates :area_atuacao, presence: true
+  validates :nome, presence: true, length: { minimum: 3, maximum: 100 }
   
-  has_many :candidaturas
-  has_many :vagas, through: :candidaturas
+  validates :telefone, presence: true, uniqueness: true, format: { with: /\A\(\d{2}\) \d{4,5}-\d{4}\z/, message: "deve estar no formato (XX) XXXXX-XXXX" }
 
+  validates :linkedin, presence: true, uniqueness: true, format: { with: /\Ahttps?:\/\/(www\.)?linkedin\.com\/.*\z/, message: "deve ser um link válido do LinkedIn" }
+
+  validates :area_atuacao, presence: true, length: { minimum: 3, maximum: 50 }
+
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  # Associações
+  has_many :candidaturas, dependent: :destroy
+  has_many :vagas, through: :candidaturas
 end
