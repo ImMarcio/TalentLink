b92b6ca04c4733a86ecf8c142b787b12d19990e2
Márcio J. Silva <105254896+ImMarcio@users.noreply.github.com>
2025-03-13 09:54:38 -0300

    feat: recuperação de senhas implementando

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,6 +15,23 @@ Rails.application.configure do
   # Enable server timing.
   config.server_timing = true
 
+
+  config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+  address: 'smtp.gmail.com',
+  port: 587,
+  domain: 'gmail.com',
+  authentication: 'plain',
+  enable_starttls_auto: true,
+  user_name: 'jose.marcio@academico.ifpb.edu.br',
+  password: '*******'
+}
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+
+
   # Enable/disable Action Controller caching. By default Action Controller caching is disabled.
   # Run rails dev:cache to toggle Action Controller caching.
   if Rails.root.join("tmp/caching-dev.txt").exist?
